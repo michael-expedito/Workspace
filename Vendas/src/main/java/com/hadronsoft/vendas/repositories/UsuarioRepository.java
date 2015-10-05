3bf23d814263030d45fa4819862efa6c60117771
Michael Expedito <michael_meas@hotmail.com>
2015-10-05 10:02:51 -0300

    Commit de Spring Framework 15.4 - finish

--- Vendas/src/main/java/com/hadronsoft/vendas/repositories/UsuarioRepository.java
+++ Vendas/src/main/java/com/hadronsoft/vendas/repositories/UsuarioRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import com.hadronsoft.vendas.model.Usuario;
 
@@ -22,6 +23,19 @@ public class UsuarioRepository implements Serializable {
 	public List<Usuario> getVendedores() {
 		return this.manager.createQuery("from Usuario", Usuario.class)
 				.getResultList();
+	}
+
+	public Usuario getByEmail(String email) {
+		Usuario usuario = null;
+		
+		try {
+			usuario = this.manager.createQuery("from Usuario where lower(email) = :email", Usuario.class)
+				.setParameter("email", email.toLowerCase()).getSingleResult();
+		} catch (NoResultException e) {
+			// nenhum usuário encontrado com o e-mail informado
+		}
+		
+		return usuario;
 	}
 
 }
--- Vendas/src/main/java/com/hadronsoft/vendas/security/AppUserDetailsService.java
+++ Vendas/src/main/java/com/hadronsoft/vendas/security/AppUserDetailsService.java
@@ -1,15 +1,50 @@
 package com.hadronsoft.vendas.security;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+
+import com.hadronsoft.vendas.model.Grupo;
+import com.hadronsoft.vendas.model.Usuario;
+import com.hadronsoft.vendas.repositories.UsuarioRepository;
+import com.hadronsoft.vendas.util.cdi.CDIServiceLocator;
+
+
 public class AppUserDetailsService implements UserDetailsService{
 
+	
+	
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UsuarioRepository usuRepository = CDIServiceLocator.getBean(UsuarioRepository.class);
+		Usuario usuario = usuRepository.getByEmail(email);
+		
+		UsuarioSistema user = null;
+		
+		if (usuario != null) {
+			user = new UsuarioSistema(usuario, getGrupos(usuario));
+		}
+		
+		return user;
+	}
+
+	private Collection<? extends GrantedAuthority> getGrupos(Usuario usuario) {
+		List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+		
+		for (Grupo grupo : usuario.getGrupos()) {
+			authorities.add(new SimpleGrantedAuthority(grupo.getNome().toUpperCase()));
+		}
+		
+		return authorities;
 	}
 
 }
